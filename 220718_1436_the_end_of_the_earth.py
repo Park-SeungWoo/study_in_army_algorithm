1f682d0a6d64a55013bc8b90e2629e98152f9c3b
Park-SeungWoo <54872527+Park-SeungWoo@users.noreply.github.com>
2022-07-18 20:39:03 +0900

    Create 220718_1436_the_end_of_the_earth.py

---
+++ 220718_1436_the_end_of_the_earth.py
@@ -0,0 +1,14 @@
+import sys
+
+def the_end_of_the_earth(num: int) -> int:
+    threesix: int = 666
+    count: int = 1
+    while True:
+        if count == num:
+            return threesix
+        threesix += 1
+        if '666' in str(threesix):
+            count += 1
+
+num = int(sys.stdin.readline())
+print(the_end_of_the_earth(num))
