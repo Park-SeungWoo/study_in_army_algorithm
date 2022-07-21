1e96c4364e88f4624cbd27b18e5d3c9b7489aa44
Park-SeungWoo <54872527+Park-SeungWoo@users.noreply.github.com>
2022-07-21 20:23:12 +0900

    Create 220721_1157_study_word.py

---
+++ 220721_1157_study_word.py
@@ -0,0 +1,13 @@
+import sys
+from collections import Counter
+
+def frequent_char(data: str) -> str:
+    chardic = Counter(data)
+    mostfreq = chardic.most_common()
+    for temp in mostfreq[1:]:
+        if mostfreq[0][1] == temp[1]:
+            return '?'
+    return mostfreq[0][0]
+
+data: str = sys.stdin.readline().rstrip().upper()
+sys.stdout.write(frequent_char(data) + '\n')
